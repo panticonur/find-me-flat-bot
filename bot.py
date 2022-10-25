054215d3ea29a69cedd72defaf0f58a4e6a32397
Andrei Sidorov <andrey.sidorov@vk.team>
2022-10-25 20:58:06 +0300

    fiz

--- bot.py
+++ bot.py
@@ -12,17 +12,19 @@ import urllib
 from sockshandler import SocksiPyHandler
 
 
-TOKEN='5712465853:AAEh9ewqzcrLwFw8PA90MAKKowR_TtpTqGk'
+BOT_TOKEN='5712465853:AAEh9ewqzcrLwFw8PA90MAKKowR_TtpTqGk'
 # pnt_flat_bot PntFlatBot
 PROXY_HOST = 'pryatki.dev'
 PROXY_PORT = 31337
 PROXY_USER = 'vasya'
 PROXY_PSWRD = '123123123'
-REQUEST_UPDATES_TIMEOUT = 45
+REQUEST_UPDATES_TIMEOUT = 5
 PARSER_DELAY = 500
 
 
-opener = urllib.request.build_opener(SocksiPyHandler(socks.SOCKS5, PROXY_HOST, PROXY_PORT, True, PROXY_USER, PROXY_PSWRD))
+#proxy_handler = SocksiPyHandler(socks.SOCKS5, PROXY_HOST, PROXY_PORT, True, PROXY_USER, PROXY_PSWRD)
+https_handler = urllib.request.HTTPSHandler()
+opener = urllib.request.build_opener(https_handler)
 patch_all()
 messages_queue = []
 data_dir = os.path.join(os.path.dirname(__file__), "data")
@@ -37,6 +39,7 @@ parser_delay = PARSER_DELAY
 parser_countdown = PARSER_DELAY
 
 debug = True
+debug2 = False
 restart = 0
 
 
@@ -55,8 +58,8 @@ def set_delay(chat_id, time):
     chats[chat_id]['delay'] = time
     save_json(chats_path, chats)
     log(u"Set chat delay {}: {}".format(chat_id, time))
-    
-    
+
+
 def remove_chat(chat_id):
     if chat_id in chats:
         tag = chats[chat_id]['tag']
@@ -67,22 +70,25 @@ def remove_chat(chat_id):
 
 
 def load_telegram_method(method, params):
-    #global debug
+    global debug
     unicode_params = {}
     for k, v in params.items():
         val = v.encode("utf8") if isinstance(v, str) else v
         unicode_params[k] = val
 
     params_str = urllib.parse.urlencode(unicode_params)
-    #token = os.environ["TG_BOT_TOKEN"]
-    url = u"https://api.telegram.org/bot{}/{}?{}".format(TOKEN,
+    url = u"https://api.telegram.org/bot{}/{}?{}".format(BOT_TOKEN,
                                                          method,
                                                          params_str)
+    if debug:
+        log(url)
     readed = opener.open(url).read()
-    #if debug:
-    #    log(u"load_telegram_method {} raw:\n".format(method)+readed)
-    return json.loads(readed, encoding="utf-8")
-    #return json.load(urllib2.urlopen(url), encoding="utf-8")
+    if debug:
+        log("load_telegram_method {}".format(method))
+    jsn = json.loads(readed)#, encoding="utf-8")
+    if debug:
+        print(jsn)
+    return jsn
 
 
 def request_updates(offset):
@@ -97,24 +103,29 @@ def send_message(chat_id, message):
     return load_telegram_method("sendMessage", params)
 
 
-def updater():
-    global debug
+def bot_updater_thread():
+    global debug, debug2
     log(u"Bot started")
     offset = 0
+
     while True:
+
         if debug:
             log(u"Updating {}".format(offset))
         try:
             updates_response = request_updates(offset)
             updates = updates_response.get("result", [])
         except Exception as e: # urllib2.HTTPError, e:
-            log(u"EXCEPTION updater:")
+            log("EXCEPTION updater:")
             print(e) #print "Unexpected error:", sys.exc_info()[0]
             if debug:
                 raise
             continue
-        #log(u"updater new data")
-        #print(updates)
+
+        if debug2:
+            log("bot_updater() new responce:")
+            print(updates)
+
         if len(updates) > 0:
             offset = updates[-1].get("update_id", offset) + 1
             message_updates = filter(
@@ -123,11 +134,11 @@ def updater():
             for upd in message_updates:
                 text = upd["message"]["text"]
                 chat_id = str(upd["message"]["chat"]["id"])
-                #log(u"got message (chat:{}): {}".format(chat_id, text))
+                log("got message (chat:{}): {}".format(chat_id, text))
                 try:
                     handle_message(chat_id, text)
                 except Exception as e:
-                    log(u"EXCEPTION handle_message (chat:{})".format(chat_id))
+                    log("EXCEPTION handle_message (chat:{})".format(chat_id))
                     print(e)
                     if debug:
                         raise
@@ -137,60 +148,63 @@ def handle_message(chat_id, message):
     global parser_countdown
     global parser_delay
     global debug
-    if message == u"/help":
-        log(u"Help")
+
+    if message == "/help":
+        log("Help")
         send_message(chat_id,
-                     u"/start <tag> <url>\n/stop\n/stat[us]\n/scan")
-    if message.find(u"/start") == 0:
-        log(u"Start")
-        parts = message.split(u" ")
+                     "/start <tag> <url>\n/stop\n/stat[us]\n/scan")
+
+    if message.find("/start") == 0:
+        log("Start")
+        parts = message.split(" ")
         if len(parts) == 3:
             add_chat(chat_id, parts[1], parts[2])
-            send_message(chat_id, u"Start scanning #"+parts[1])
+            send_message(chat_id, "Start scanning #"+parts[1])
             parser_countdown = 2
         else:
-            send_message(chat_id, u"Usage: /start <tag> <cian_url>")
-    elif message == u"/stop":
-        log(u"Stop")
+            send_message(chat_id, "Usage: /start <tag> <cian_url>")
+
+    elif message == "/stop":
+        log("Stop")
         chat = chats.get(chat_id, False)
         tag = remove_chat(chat_id)
-        if tag:
-            send_message(chat_id, u"Stop scanning #{}\n{}".format(tag, chat['url']))
-    if message == u"/stat":
-        log(u"Stat")
+        send_message(chat_id, "Stop scanning #{}\n{}".format(tag, chat['url']))
+
+    if message == "/stat":
+        log("Stat")
         chat = chats.get(chat_id, False)
-        msg = u"chat not found!"
+        msg = "chat not found!"
         if chat:
-            msg = u"#{}  delay: {} {}".format(
+            msg = "#{}  delay: {} {}".format(
                 chat['tag'], parser_delay, parser_countdown)
         log(msg)
         send_message(chat_id, msg)
-    if message == u"/status":
+    if message == "/status":
         if debug:
-            log(u"Status  {}".format(json.dumps(chats)))
+            log("Status  {}".format(json.dumps(chats)))
         else:
-            log(u"Status")
+            log("Status")
         chat = chats.get(chat_id, False)
-        msg = u"chat not found!\ndebug:"+str(debug)
+        msg = "chat not found!\ndebug:"+str(debug)
         if chat:
-            msg = u"#{}  delay: {} {}  debug: {}\n{}".format(
+            msg = "#{}  delay: {} {}  debug: {}\n{}".format(
                 chat['tag'], parser_delay, parser_countdown, debug, chat['url'])
         log(msg)
         send_message(chat_id, msg)
-    if message == u"/clear":
-        log(u"Clear")
+    if message == "/clear":
+        log("Clear")
         try:
             os.remove(chats_path)
         except:
-            log(u"error remove "+chats_path)
+            log("error remove "+chats_path)
         try:
             os.remove(known_path)
         except:
-            log(u"error remove "+known_path)
+            log("error remove "+known_path)
         try:
             os.remove(page_path)
         except:
-            log(u"error remove "+page_path)
+            log("error remove "+page_path)
         send_message(chat_id, u"files removed")
     if message == u"/debug":
         debug = not debug
@@ -198,9 +212,11 @@ def handle_message(chat_id, message):
         msg = u"Debug " + (u"enabled" if debug else u"disabled")
         log(msg)
         send_message(chat_id, msg)
+
     if message == u"/scan":
         parser_countdown = 1
-        log(u"Scan")
+        log("Scan")
+
     if message == u"/restart":
         log(u"Restart")
         global restart
@@ -226,44 +242,46 @@ def handle_message(chat_id, message):
             send_message(chat_id, u"Usage: /delay <seconds>")                 
 
 
-def parser():
+def cian_parser_thread():
     global parser_countdown
     global parser_delay
+
     while True:
         chats_copy = chats.copy()
+
         if len(chats_copy)==0:
             log(u"sleep: {}s".format(parser_delay))
             parser_countdown = parser_delay
             while parser_countdown>0:
-                if len(chats)!=0:
-                    break
                 gevent.sleep(1)
                 parser_countdown -= 1
+
         for chat_id, chat in chats_copy.items():
             parser_countdown = parser_delay
             while parser_countdown>0:
                 gevent.sleep(1)
                 parser_countdown -= 1
-            log(u"Parsing {} {}".format(chat_id, chat['url' if debug else 'tag']))
-            refs, page_links = cian.parse(known_path, chat['url'])
-            if refs is None:
-                continue
-            #log(u"  parsed {}, page_links {}".format(len(refs), page_links))
+
+            log("{ parsing {} {}".format(chat_id, chat['url' if debug else 'tag']))
+            hata_refs, onpage_links_count = cian.parse(known_path, chat['url'])
+            log("} parsed {}, total_onpage_links {}".format(len(hata_refs), onpage_links_count))
             if debug:
-                print(refs)
-            msg = False
-            if len(refs) > 0:
-                for ref in refs:
-                    #msg = u"#"+chat['tag']+u"\n"+ref
-                    msg = ref
-                    send_message(chat_id, msg)
-                    log(u"  parser: "+msg)
+                print(hata_refs)
+            if hata_refs is None: # fail
+                continue
+
+            if len(hata_refs) > 0:
+                for ref in hata_refs:
+                    send_message(chat_id, ref)
+                    log("  parser: "+ref)
+
             elif debug:
-                msg = u"no new refs #"+chats[chat_id]['tag']
+                msg = "no new hata-refs #"+chats[chat_id]['tag']
                 send_message(chat_id, msg)
-                log(u"  parser: "+msg)
-            if page_links<1:
-                msg = u"Warning!\nGot no links on the page #{}\nCheck CIAN captcha!".format(
+                log("  parser: "+msg)
+
+            if onpage_links_count<1:
+                msg = "Warning!\nGot no links on the page #{}\nCheck CIAN captcha!".format(
                                 chats[chat_id]['tag'])
                 send_message(chat_id, msg)
                 log(u"  parser: "+msg)
@@ -271,8 +289,8 @@ def parser():
 
 def main():
     gevent.joinall([
-        gevent.spawn(updater),
-        gevent.spawn(parser),
+        gevent.spawn(bot_updater_thread),
+        gevent.spawn(cian_parser_thread),
     ])
 
 if __name__ == "__main__":
